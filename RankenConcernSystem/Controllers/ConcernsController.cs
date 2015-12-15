b260d95c805e2156cd8a544502b7440ec95f3194
malkurwi <mohamed_alkurwi@insideranken.org>
2015-12-15 10:02:44 -0600

    Added Model & Scaffolding

---
+++ RankenConcernSystem/Controllers/ConcernsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RankenConcernSystem.Models;
+
+namespace RankenConcernSystem.Controllers
+{
+    public class ConcernsController : Controller
+    {
+        private RankenConcernSystemContext db = new RankenConcernSystemContext();
+
+        // GET: Concerns
+        public ActionResult Index()
+        {
+            return View(db.Concerns.ToList());
+        }
+
+        // GET: Concerns/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Concerns concerns = db.Concerns.Find(id);
+            if (concerns == null)
+            {
+                return HttpNotFound();
+            }
+            return View(concerns);
+        }
+
+        // GET: Concerns/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Concerns/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,DateOfMake,ConMakerName,GetRelationship,GetReportingMethod,StudentID,ConMakerPhone,ConMakerEmail,ReasonConMade,NameConRecieved,Details,ActionMade,NameOfActionMaker,ResultsOfCon,DateOfResult")] Concerns concerns)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Concerns.Add(concerns);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(concerns);
+        }
+
+        // GET: Concerns/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Concerns concerns = db.Concerns.Find(id);
+            if (concerns == null)
+            {
+                return HttpNotFound();
+            }
+            return View(concerns);
+        }
+
+        // POST: Concerns/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,DateOfMake,ConMakerName,GetRelationship,GetReportingMethod,StudentID,ConMakerPhone,ConMakerEmail,ReasonConMade,NameConRecieved,Details,ActionMade,NameOfActionMaker,ResultsOfCon,DateOfResult")] Concerns concerns)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(concerns).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(concerns);
+        }
+
+        // GET: Concerns/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Concerns concerns = db.Concerns.Find(id);
+            if (concerns == null)
+            {
+                return HttpNotFound();
+            }
+            return View(concerns);
+        }
+
+        // POST: Concerns/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Concerns concerns = db.Concerns.Find(id);
+            db.Concerns.Remove(concerns);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RankenConcernSystem/Models/Concerns.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RankenConcernSystem.Models
+{
+    public class Concerns
+    {
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Date Recieved")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfMake { get; set; }
+
+        [Required]
+        [Display(Name = "Name Of Concern Maker")]
+        public string ConMakerName { get; set; }
+
+
+        public enum Relationships { Student, Parent, Employee, Faculty, Other }
+        [Required]
+        [Display(Name = "Relationship")]
+        public Relationships GetRelationship { get; set; }
+
+
+        public enum Methods { Phone, Emial, Letter, In_Person, Through_HLC }
+
+        [Display(Name = "Concern Made Via")]
+        [Required]
+        public Methods GetReportingMethod { get; set; }
+
+
+        [Display(Name = "Student-ID")]
+        public string StudentID { get; set; }
+
+
+
+        [Display(Name = "Phone")]
+        [Required]
+        public string ConMakerPhone { get; set; }
+
+        [Display(Name = "E-mail")]
+        [Required]
+        public string ConMakerEmail { get; set; }
+
+        [Display(Name = "Reason")]
+        [StringLength(50, MinimumLength = 3)]
+        [Required]
+        public string ReasonConMade { get; set; }
+
+        [Display(Name = "Reported To")]
+        [Required]
+        public string NameConRecieved { get; set; }
+
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Details { get; set; }
+
+        [Display(Name = "Action")]
+        [Required]
+        public string ActionMade { get; set; }
+
+        [Display(Name = "Action Made By")]
+        [Required]
+        public string NameOfActionMaker { get; set; }
+
+        [Display(Name = "Results")]
+        [Required]
+        public string ResultsOfCon { get; set; }
+
+        [Display(Name = "Result Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Required]
+        public DateTime DateOfResult { get; set; }
+    }
+}
